efdbbc89c461e9781890465b083b77c35be2ab77
sahil1sk <khannasahil303@gmail.com>
2020-11-14 06:59:34 +0530

    ListView and ListView.Builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,64 +4,63 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  final List<Transaction> transaction;
+  final List<Transaction> transactions;
 
-  TransactionList(this.transaction);
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300, // we use height for set singleChildScrollView so that this will confine the height where to set the scroller
-      child: SingleChildScrollView(
-        child: Column(     // we don't use ... because we make list from class it's already a copy
-          children: transaction.map((tx) {
-            return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10, 
-                        horizontal: 10
+      height: 300, 
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10, 
+                    horizontal: 10
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
                       ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                      '\$${transactions[index].amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
                       ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                          '\$${tx.amount}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
                     ),
                   ],
                 ),
-              );
-          }).toList(),
-        ),
+              ],
+            ),
+          );
+        },     
+        itemCount: transactions.length,
       ),
     );
   }
