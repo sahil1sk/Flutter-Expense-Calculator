be7b9a9fa6069874f0508bc5fe3e94b6bc4fe19e
sahil1sk <khannasahil303@gmail.com>
2020-11-18 07:08:04 +0530

    Problem With Lists

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math'; // for using Random
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -12,6 +14,27 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTx;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+
+  @override
+  void initState() {
+    super.initState();
+    const availableColors = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)]; // generate radndom number b/w 0,1,2,3
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +46,25 @@ class TransactionItem extends StatelessWidget {
       elevation: 5,
       child: ListTile( // helps to make easy styled element
         leading: CircleAvatar( // leading is the first part
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6),
             child: FittedBox( // means the data will be set into it's our box only srink the size if the number is large not go to another line
-              child: Text('\$${transaction.amount}'),
+              child: Text('\$${widget.transaction.amount}'),
             ),
           ),
         ),
         title: Text( // setting the title
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.title, // setting the style of text which we configure at theme
         ),
         subtitle: Text( // helps to set the subtitle
-          DateFormat.yMMMd().format(transaction.date),
+          DateFormat.yMMMd().format(widget.transaction.date),
         ), // trailing will help to show the last side element like leading showing first
         trailing: MediaQuery.of(context).size.width > 460 ? 
         FlatButton.icon(
-          onPressed: () => deleteTx(transaction.id), 
+          onPressed: () => widget.deleteTx(widget.transaction.id), 
           icon: const Icon(Icons.delete), // use const if widget remain same so that it will not rebuild again when the build method is called
           label: const Text('Delete'),
           textColor: Theme.of(context).errorColor,
@@ -48,7 +72,7 @@ class TransactionItem extends StatelessWidget {
         :IconButton(
           icon: Icon(Icons.delete),
           color: Theme.of(context).errorColor, // getting the error color from the theme
-          onPressed: () => deleteTx(transaction.id),
+          onPressed: () => widget.deleteTx(widget.transaction.id),
         ),
       ),
     );
